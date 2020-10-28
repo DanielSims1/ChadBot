72562a71959092f9e0d19073b6d19f1fe94abed1
Daniel Sims <danielssims@hotmail.com>
2020-10-28 19:10:34 -0400

    potential commands

---
+++ chadbot.py
@@ -0,0 +1,58 @@
+"""
+ChadBot is an exploration of the Discord Bot API which utilizes information taken from the Official Tarkov Wiki to fuffill user requests
+    Potential prefixs
+        ! , . , > , / , // , $ , % , ^ , & , * , - , + , = , _ , : , \  
+
+    List of Commands:
+
+        !keys <map> -b[est] -u[seless]
+        !bestkeys -m[ap] <map_name>
+        !key <key_name> 
+        !uselesskeys
+
+        !useful <name>
+
+        !best <gun> -r[ecoil] -e[rgonomics]
+            !recoil <gun>
+            !ergo <gun>
+        !med <name>
+        !stim <name>
+
+        !map <name>
+        !maps
+
+        !trader -n[ame] <level> 
+        !prapor !therapist !pk !jaeger !mechanic !ragman <level> 
+
+        !hideout <station>
+        !hideoutitems
+
+        !quests <quest_name)
+        !kappa
+
+        !keeplist -h[ideout] -q[uests]
+
+        !ammochart
+        !ammo -g[un] <gun_name> -c[aliber] <round_caliber>
+
+        !armor <name>
+
+        !sellorder
+        !bitcoin !btc 
+        
+        !cheeky 
+        !dicky
+
+        !chadloadout
+
+        !chadtip
+
+        !rattip
+
+        !value <item>
+
+        !wiki <query>
+
+    
+"""
+
