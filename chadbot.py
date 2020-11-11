c461db8cba288a1d6070373b6e0402d3c72d1ad1
Daniel Sims <danielssims@hotmail.com>
2020-11-11 18:27:44 -0500

    start of best command

--- chadbot.py
+++ chadbot.py
@@ -70,20 +70,40 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 TEST_GUILD = os.getenv('DISCORD_TEST_GUILD')
 GUILD = os.getenv('DISCORD_GUILD')
 
+
+#This could be a mapping of gun names to screenshots of meta builds. screenshots could potentially be stored on this device and sent with the message.
+known_gun_names = {'hk416a5':"hk416a5.png", 'hk':"hk416a5.png", 'hk416': "hk416a1.png"} #, 'm4a1', 'SA-58', 'ak-74m', 'ash-12', 'as val', 'kel-tec rfb', '.308 mdr', '5.56 mdr', 'sr-25', 'akm', 'rpk-16'}
+# force queries to lowercase to ensure they get correct build
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-bot.run(TOKEN)
-
-@bot.command(name='cheeki', help = "Help yourself cyka")
+@bot.command(name= 'cheeki', help = "Help yourself cyka")
 async def cheeki(ctx):
     await ctx.send("breeki")
+
+@bot.command(name='best', help = "Gives you the best meta build for a given gun, preferring recoil")
+async def best_gun(ctx, gun_name):
+    # force to lowercase to make queries easier
+    formatted_gun_name = gun_name.lower()
+    # assuming no *recoil* or *ergonomics* modifier
+    if formatted_gun_name in known_gun_names.keys():
+        # get file from correct location
+        gun_image = discord.File(f"images/best/{known_gun_names.get(formatted_gun_name)}")
+        await ctx.send("Here is the best build for the {}, enjoy comrade!".format(formatted_gun_name))
+        await ctx.send(file=gun_image)
+
+    else:
+        await ctx.send(f"The gun *{gun_name}* currently lacks meta builds.... make sure you typed the name correctly")
+
 
 @bot.command(name='ammo', help = "Gives the best 3 ammo types for a given caliber, or a table of the best ammo for each caliber")
 async def ammo(ctx):
     await ctx.send("Nofoodaftermidnight ammo spreadsheet --> https://docs.google.com/spreadsheets/d/1jjWcIue0_PCsbLQAiL5VrIulPK8SzM5jjiCMx9zUuvE/edit#gid=64053005")
 
 @bot.command(name='bestkeys', help = "Gives the best keys for a given map, or a table of all the best keys if no map is given")
+async def bestkeys(ctx):
+    await ctx.send("out of date best keys --> https://preview.redd.it/let6rgrtq8951.png?width=960&crop=smart&auto=webp&s=bb6ecc50549e3dc4c7e9d6e75529820613d003b1")
 
 bot.run(TOKEN)
